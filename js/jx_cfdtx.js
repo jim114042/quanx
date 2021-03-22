32978fbba0117dc0138d9a389116f80aa8473474
jim114042 <114042015@qq.com>
2021-03-22 12:47:35 +0800

    Update jx_cfdtx.js

--- js/jx_cfdtx.js
+++ js/jx_cfdtx.js
@@ -90,7 +90,7 @@ function getCookies() {
     $.cookieArr = Object.values(jdCookieNode);
   } else {
     const CookiesJd = JSON.parse($.getdata("CookiesJD") || "[]").filter(x => !!x).map(x => x.cookie);
-    $.cookieArr = [$.getdata("CookieJD") || "", $.getdata("CookieJD2") || "", ...CookiesJd];
+    $.cookieArr = [$.getdata("CookieJD") || "", $.getdata("CookieJD2") || "", ...CookiesJd].filter(item=>!!item);
   }
   if (!$.cookieArr[0]) {
     $.msg(
