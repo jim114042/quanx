ef6865b91eb823e8a4ad041bbbe3fd9bbe361e9f
jim114042 <114042015@qq.com>
2021-05-16 18:40:51 +0800

    Update dyjsb.js

--- js/dyjsb.js
+++ js/dyjsb.js
@@ -262,7 +262,7 @@ if (!signheaderArr[0]) {
       console.log(`\n开始【抖音极速版${$.index}】`)
 	    
       let yck = Math.floor(Math.random() * 60)
-      if(Math.floor(Math.random() * 10)<3){ //大概10次中3次看视频
+      if(Math.floor(Math.random() * 30)<5){ //大概10次中3次看视频
          console.log(`延迟${yck}秒看视频`)	 
 	 setTimeout(async ()=>{
 	   await watch_video()
