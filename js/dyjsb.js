7586f90a52395a12ada26a38c7275170a7423df7
jim114042 <114042015@qq.com>
2021-06-15 14:18:36 +0800

    Update dyjsb.js

--- js/dyjsb.js
+++ js/dyjsb.js
@@ -279,7 +279,7 @@ if (!signheaderArr[0]) {
 	await showmsg();
         return;
       }
-      if(Math.floor(Math.random() * 10)>=7){ //大概10次中3次跳过
+      if(Math.floor(Math.random() * 10)>=6){ //大概10次中4次跳过
 	$.log('跳过宝盒')
 	return;
       }
